ac9bcd99eddb5670722a0b94b1a4a6726f66e050
Ivik <andoidxx@gmail.com>
2016-05-28 16:32:45 +0300

    creating models Account, Message

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Message.create(title: 'hello_balance',
+               text: 'Здравствуйте, {{name}}. Ваш баланс: {{balance}}{{currency}}')
+
+Account.create(name: 'Chuck Norris',
+               balance: 1_000_000,
+               currency: 'USD')
+
+Account.create(name: 'Леонид Якубович',
+               balance: 100,
+               currency: 'RUB')
